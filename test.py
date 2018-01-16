c4d64ed48474d4f14d90a000dbfaf137da4d72b7
Liu Shuai <957419821@qq.com>
2018-01-16 11:06:48 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 import numpy as np
 from dataset import *
+from dataSetTools import tools
+import csv
 
-dataset = DataSet()
-print([dataset.width, dataset.height])
+batchStart = 0
+batchEnd = 10
+datas = tools.getData('orl_faces/', batchStart, batchEnd)
+out = open("datas1.csv", 'w', newline='')
+writer = csv.writer(out, dialect='excel')
+for r in datas:
+    writer.writerow(r)
